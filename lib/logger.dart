06d5d5489e18cb03eb8c32b529a53b71c7b14e2a
Vantim Arzanidis <74730689+blablabla123-cell@users.noreply.github.com>
2024-08-18 12:17:12 +0300

    Commit

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/foundation.dart';
 
+enum LogType {
+  info('INFO'),
+  warning('WARNING');
+
+  final String text;
+
+  const LogType(this.text);
+}
+
+
 class Logger {
-  void log(String message) {
+  void log(String message, {LogType logType = LogType.info}) {
     // Check if message is Empty
     assert(message.isNotEmpty);
 
     if (kDebugMode) {
-      print('[LOG] - $message');
+      print('[${logType.text}] - $message');
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,9 @@
 import 'package:meni/logger.dart';
 
-void main() {
-
+void main() async {
   final Logger logger = Logger();
 
-  logger.log(calculate('5').toString());
-  
+  logger.log('Message');
 }
 
-int calculate<T>(T number) {
 
-  int value = 0;
-
-  if(number is String) {
-    value = int.tryParse(number as String) ?? 0;
-    // Convert String to Int
-  }
-
-  if(number is double) {
-    // Convert double to Int
-    value = number.toInt();
-  }
-
-  return value * value;
-}
