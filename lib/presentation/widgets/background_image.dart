46a42113382a196654ccb0991cc3b5816274b9bd
Meirzhan-student <63948299+Meirzhan-student@users.noreply.github.com>
2024-09-07 03:26:59 +0500

    я старлсяб только back_image

--- lib/presentation/widgets/background_image.dart
+++ lib/presentation/widgets/background_image.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class BackgroundImage extends StatelessWidget {
-  const BackgroundImage(this.text, {super.key});
+  const BackgroundImage(this.text, {this.imageUrl, super.key});
   final String text;
+  final String? imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      const Icon(Icons.account_circle),
-      Text(text),
-    ]);
+    return Container(
+      width: double.infinity,
+      height: 200,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+              image: NetworkImage(imageUrl!),
+              fit: BoxFit.cover
+          ),
+      ),
+    );
   }
 }
