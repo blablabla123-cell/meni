8e0ef3fd30ab37a911976fbbe30f6cff700aed7f
Meirzhan-student <63948299+Meirzhan-student@users.noreply.github.com>
2024-09-14 03:30:44 +0500

    чкт у меня не запустилось, но я запушу

--- lib/presentation/boarding/boarding_screen.dart
+++ lib/presentation/boarding/boarding_screen.dart
@@ -15,21 +15,45 @@ class _BoardingScreenState extends State<BoardingScreen> {
 
   final Logger logger = Logger();
   String name = 'Meirzhan';
+  String imageUrl = 'https://img.freepik.com/free-photo/gradient-iphone-wallpaper-oil-bubble-water-background_53876-176849.jpg?w=740&t=st=1726265909~exp=1726266509~hmac=96af254e144e22bf45ad06b53c04bdacc86c803e03b65fec54fcdfd0992d739c';
 
   @override
   Widget build(BuildContext context) {
     logger.log('Build');
 
-    return Column(
-      children: <Widget>[
-        Text('Welcome! $name'),
-        TextButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Well Cum'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Welcome $name' ,
+            style: const TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          ),
+          TextButton(
           onPressed: () => setState(() => name = 'Vadim'),
           child: const Text('Skip'),
-        ),
-        BackgroundImage(name),
-      ],
+       ),
+       BackgroundImage(
+        name,
+        imageUrl: imageUrl,
+       )
+        ],
+      ),
     );
+
+
+   // return Column(
+    //  children: <Widget>[
+     //   Text('Welcome! $name'),
+     //   TextButton(
+     //     onPressed: () => setState(() => name = 'Vadim'),
+    //      child: const Text('Skip'),
+    //    ),
+   //     BackgroundImage(name),
+   //   ],
+  //  );
   }
 }
 
