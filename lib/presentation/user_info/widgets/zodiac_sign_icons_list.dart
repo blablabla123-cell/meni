fe810f95e2fbaeeaf464c2578e160f77f09bdf45
Vantim Arzanidis <74730689+blablabla123-cell@users.noreply.github.com>
2024-10-26 13:09:24 +0300

    selectir

--- lib/presentation/user_info/widgets/zodiac_sign_icons_list.dart
+++ lib/presentation/user_info/widgets/zodiac_sign_icons_list.dart
@@ -1,51 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:meni/presentation/user_info/data/models/zodiac_sign.dart';
 
-class Zodiacs extends StatefulWidget {
+class Zodiacs extends StatelessWidget {
   const Zodiacs({required this.zodiacs, required this.selectedIndex, super.key});
 
   final List<ZodiacSign> zodiacs;
   final int selectedIndex;
 
   @override
-  State<Zodiacs> createState() => _ZodiacsState();
-}
-
-class _ZodiacsState extends State<Zodiacs> {
-  final ScrollController controller = ScrollController();
-
-  @override
-  void didUpdateWidget(covariant Zodiacs oldWidget) {
-   
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: ListView.separated(
-        controller: controller,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context, int index) {
-          final bool isSelected = widget.selectedIndex == index;
-
-          return Text(
-            widget.zodiacs[index].icon,
-            style: TextStyle(fontSize: isSelected ? 64 : 32),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const SizedBox(width: 16);
-        },
-        itemCount: widget.zodiacs.length,
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        // [1, 2, 3, 4, 5]
+        ...List<Text>.generate(2, (int index) {
+          return Text(_getRandom().icon, style: const TextStyle(fontSize: 48));
+        }),
+        Text(
+          zodiacs[selectedIndex].icon,
+          style: const TextStyle(fontSize: 72),
+        ),
+        ...List<Text>.generate(2, (int index) {
+          return Text(_getRandom().icon, style: const TextStyle(fontSize: 48));
+        }),
+      ],
     );
+  }
+
+  ZodiacSign _getRandom() {
+    zodiacs.shuffle();
+
+    return zodiacs.first == zodiacs[selectedIndex] ? zodiacs.last : zodiacs.first;
   }
 }
