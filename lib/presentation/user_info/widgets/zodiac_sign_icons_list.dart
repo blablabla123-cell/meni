8d01ba8db5a52a67acc263e59e2c3edb7c3d0855
Vantim Arzanidis <74730689+blablabla123-cell@users.noreply.github.com>
2024-10-26 14:31:10 +0300

    ss

--- lib/presentation/user_info/widgets/zodiac_sign_icons_list.dart
+++ lib/presentation/user_info/widgets/zodiac_sign_icons_list.dart
@@ -1,35 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:meni/presentation/user_info/data/models/zodiac_sign.dart';
 
-class Zodiacs extends StatelessWidget {
+class Zodiacs extends StatefulWidget {
   const Zodiacs({required this.zodiacs, required this.selectedIndex, super.key});
 
   final List<ZodiacSign> zodiacs;
   final int selectedIndex;
 
   @override
+  State<Zodiacs> createState() => _ZodiacsState();
+}
+
+class _ZodiacsState extends State<Zodiacs> {
+  late List<ZodiacSign> localZodiacs;
+
+  @override
+  void initState() {
+    super.initState();
+    _update();
+  }
+
+  @override
+  void didUpdateWidget(covariant Zodiacs oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.selectedIndex != widget.selectedIndex) {
+      _update();
+    }
+  }
+
+  void _update() {
+    // Create a new list excluding the selected zodiac sign
+    localZodiacs = List<ZodiacSign>.from(widget.zodiacs);
+
+    final ZodiacSign selected = localZodiacs[widget.selectedIndex];
+
+    localZodiacs.removeAt(widget.selectedIndex);
+
+    localZodiacs.shuffle();
+
+    localZodiacs.insert(2, selected);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        // [1, 2, 3, 4, 5]
-        ...List<Text>.generate(2, (int index) {
-          return Text(_getRandom().icon, style: const TextStyle(fontSize: 48));
-        }),
-        Text(
-          zodiacs[selectedIndex].icon,
-          style: const TextStyle(fontSize: 72),
+      children: List<Widget>.generate(
+        5,
+        (int index) => Text(
+          localZodiacs[index].icon,
+          style: TextStyle(fontSize: index == 2 ? 64 : 48),
         ),
-        ...List<Text>.generate(2, (int index) {
-          return Text(_getRandom().icon, style: const TextStyle(fontSize: 48));
-        }),
-      ],
+      ),
     );
-  }
-
-  ZodiacSign _getRandom() {
-    zodiacs.shuffle();
-
-    return zodiacs.first == zodiacs[selectedIndex] ? zodiacs.last : zodiacs.first;
   }
 }
